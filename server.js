43cfdba536d5c595f162c6ac3712ef8464fe9bff
Ahmidi Yasser <88626454+dexter21767@users.noreply.github.com>
2022-10-31 16:56:14 +0100

    first commit

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 63355})
 
 // when you've deployed your addon, un-comment this line
-//publishToCentral("https://2ecbbd610840-cinestream.baby-beamup.club/manifest.json")
+publishToCentral("https://2ecbbd610840-cinestream.baby-beamup.club/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
