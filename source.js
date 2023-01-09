74c23f26e7fcc3e4b3fea31abacdd109930b0572
Ahmidi Yasser <88626454+dexter21767@users.noreply.github.com>
2023-01-09 14:00:31 +0100

    add nollyverse

--- source.js
+++ source.js
@@ -1,7 +1,10 @@
 const log = require('./logger');
 const consumet = require('./lib/consumet');
+const noyyverse = require('./lib/nollyverse')
+const sources = require('./lib/sources')
 
 const NodeCache = require("node-cache");
+const { restart } = require('nodemon');
 const StreamCache = new NodeCache({ stdTTL: (6 * 60 * 60), checkperiod: (3 * 60 * 60) });
 
  
@@ -12,19 +15,34 @@ async function stream(type, id) {
         console.log("stream", type, id)
         log.info("stream: "+ type +' '+id)
         let streams;
+        const promises=[];
         if (id.match(/tt\d+(:\d+)?(:\d+)?/i)){
             const [tmdb_id,season,episode] = id.split(":");
-            
+            for (const key in sources.movies) {
+                promises.push(sources.movies[key](type,tmdb_id, episode, season).catch(e => { console.error(e) }));
+            }  
             streams = await consumet.Movie(type,tmdb_id,episode,season)
 
         }
         else if(id.match(/kitsu:\d+(:\d+)?/i)){
             const [kitsu_id,episode] = id.match(/\d+/ig);
             console.log(kitsu_id,episode)
+            for (const key in sources.anime) {
+                promises.push(sources.anime[key](kitsu_id,episode).catch(e => { console.error(e) }));
+            }  
             streams = await consumet.Anime(kitsu_id,episode)
 
         }
-        
+        streams = await Promise.allSettled(promises).then(promises=>{
+            let streams = []
+            promises.forEach(({status,value})=>{
+                if(status == "fulfilled"){
+                    streams = streams.concat(value)
+                } 
+            })
+            return streams
+            
+        });
         if (streams) StreamCache.set(id, streams)
         return streams
     } catch (e) {
