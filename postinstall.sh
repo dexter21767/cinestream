8d154a4c6d02dcb4b536ef755c7c5aa7442978f3
dexter21767 <ahmidiyasser@gmail.com>
2023-03-07 17:32:45 +0100

    Auto-commit

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --location --output beamup-logger https://github.com/dexter21767/beamup-logger/releases/download/V0.1.4/beamup-logger-node16-linux
+#curl --location --output beamup-logger https://github.com/dexter21767/beamup-logger/releases/download/V0.1.4/beamup-logger-node16-linux
 
-#curl --location --output beamup-logger https://github.com/dexter21767/beamup-logger/releases/latest/download/beamup-logger-node16-linux
+curl --location --output beamup-logger https://github.com/dexter21767/beamup-logger/releases/latest/download/beamup-logger-node16-linux
 chmod +x beamup-logger
